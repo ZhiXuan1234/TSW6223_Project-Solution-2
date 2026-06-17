0d2430ecd6de145eba3555825d2ce888e97ef6b0
ZhiXuan1234 <164007596+ZhiXuan1234@users.noreply.github.com>
2026-06-17 17:29:13 +0800

    Create parse_xml.py

---
+++ parse_xml.py
@@ -0,0 +1,7 @@
+# The purpose of this code file:
+# 1. Read career_skill_data.xml
+# 2. Extract skills, careers, courses, and students
+# 3. Print them in a readable format
+# 4. Confirm Python can correctly understand the XML structure before we convert it to RDF/RDFS
+
+
